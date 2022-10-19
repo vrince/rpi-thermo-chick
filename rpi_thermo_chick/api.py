b038f24e46a61d26f8cf6f169ce314c11c4be31e
Thomas Vincent <vrince@gmail.com>
2022-10-18 21:02:37 -0400

    remove flat file history + use influxbd query instead

--- rpi_thermo_chick/api.py
+++ rpi_thermo_chick/api.py
@@ -16,7 +16,7 @@ from appdirs import user_config_dir
 from rpi_thermo_chick import logger
 from rpi_thermo_chick.sensors import read_temperature
 from rpi_thermo_chick.relays import init_gpio, set_relay
-from rpi_thermo_chick.influxdb import init_influxdb_client, write_to_influxdb
+from rpi_thermo_chick.influxdb import init_influxdb_client, write_to_influxdb, query_mean
 
 host = environ.get('RPI_THERMO_CHICK_HOST', '0.0.0.0')
 port = environ.get('RPI_THERMO_CHICK_PORT', '8000')
@@ -29,14 +29,6 @@ vue_app = open( module_dir + '/index.html', 'r').read()
 def now_ts():
     return datetime.datetime.now().isoformat()
 
-def get_history_index(date, interval, intervalPerDay):
-    seconds_since_midnight = (date - date.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
-    return max(0,min(int(seconds_since_midnight / interval), intervalPerDay))
-
-def rotate(arr,d):
-    result = arr[d:len(arr)]+arr[0:d]
-    return result
-
 def apply_thermostat():
     inside_temp = thermometers[0]['temp']
     if inside_temp < target_temperature:
@@ -44,34 +36,18 @@ def apply_thermostat():
     elif inside_temp >= target_temperature + 1:
         update_relay(0, 0)
 
-def update_min_max_temperature():
-    global thermometers
-    for i, _ in enumerate(thermometers):
-        thermometers[i]['min'] = min([e for e in histories[i] if e is not None])
-        thermometers[i]['max'] = max([e for e in histories[i] if e is not None])
-
 def update():
     global thermometers, histories, ts, index
-    now = datetime.datetime.now()
-    last_push = now
     while not stopped:
         ts = now_ts()
-        now = datetime.datetime.now()
-        index = get_history_index(now,interval, intervalPerDay)
         fields = {}
         for i, t in enumerate(thermometers):
             temp = read_temperature(t['device'])
-            fields[str(i)] = temp
-            influxdb.get('bucket', 'rpi_thermo_chick')
+            fields[thermometers[i].get('name',str(i))] = temp
             if temp is not None:
                 thermometers[i]['temp'] = temp
                 thermometers[i]['ts'] = now_ts()
-                histories[i][index] = temp
-        write_to_influxdb({'fields': fields, 'tags': {'project':'🐔🔥'}})
-        if (now - last_push).total_seconds() > 120:
-            last_push = now
-            update_min_max_temperature()
-            push_history(histories)
+        write_to_influxdb(fields=fields)
         apply_thermostat()
         sleep(10)
 
@@ -79,21 +55,6 @@ def update_relay(relay_id, state):
     relays[relay_id]['on'] = state
     relays[relay_id]['ts'] = now_ts()
     set_relay(relays[relay_id]['pin'], state)
-
-def pull_history():
-    try:
-        with open(config_dir + '/histories.json', 'r') as f:
-            return json.load(f)
-    except:
-        logger.error('cannot pull history')
-
-
-def push_history(h):
-    try:
-        with open(config_dir + '/histories.json', 'w') as f:
-            json.dump(h, f)
-    except:
-        logger.error('cannot push history')
 
 # state
 ts = now_ts()
@@ -110,10 +71,8 @@ influxdb = {}
 
 def init(config_file=''):
     global config, relays, thermometers, histories, initialized, influxdb
-
     if not config_file:
         config_file = config_dir + '/config.json'
-
     with open(config_file, 'r') as f:
         config = json.load(f)
 
@@ -129,14 +88,6 @@ def init(config_file=''):
         r.update({'on':0, 'ts': now_ts()})
     for t in thermometers:
         t.update({'temp':0, 'ts': now_ts(), 'min': 0, 'max': 0})
-
-    # read back historical data
-    try:
-        histories = pull_history()
-        update_min_max_temperature()
-    except:
-        histories = [[None] * intervalPerDay, [None] * intervalPerDay]
-        push_history(histories)
 
     # setup GPIO
     if not initialized:
@@ -157,7 +108,6 @@ def read_root():
         'relays': relays, 
         'thermometers': thermometers,
         'ts': ts,
-        'index': index, 
         'target_temperature': target_temperature
         }
 
@@ -168,12 +118,9 @@ def read_vue_app():
 
 @app.get('/relay/{relay_id}/{action}')
 def read_item(relay_id: int, action: str = 'on'):
-
     valid_ids = range(0, len(relays)-1)
-
     if relay_id not in valid_ids:
         return  {'ok': False, 'msg': f'relay_id must be in {valid_ids}'}
-
     if action not in ['on','off', '1', '0']:
         return  {'ok': False, 'msg': 'action must be \'on\' or \'off\''}
 
@@ -182,13 +129,14 @@ def read_item(relay_id: int, action: str = 'on'):
 
 @app.get('/chart')
 def read_history():
+    timestamp, inside = query_mean(field='inside', window='15m', range='24h')
+    _, outside = query_mean(field='outside', window='15m', range='24h')
     return {
         'rpi-thermo-chick': '🐔🔥',
         'ok': True,
         'ts': ts,
-        'index': index,
-        'labels': [ f'{int(i/4)}h' if i % 4 == 0 else '' for i in range(intervalPerDay,0,-1)],
-        'datasets': [rotate(histories[0], index+1), rotate(histories[1], index+1)]
+        'labels': timestamp,
+        'datasets': [inside, outside]
         }
 
 @app.get('/target/{temperature}')
--- rpi_thermo_chick/influxdb.py
+++ rpi_thermo_chick/influxdb.py
@@ -20,13 +20,37 @@ def init_influxdb_client(config):
         logger.info(f'cannot create influxdb client, exception({e})')
 
 
-def write_to_influxdb(point, bucket='rpi_thermo_chick', org='org'):
+def write_to_influxdb(name='tempSensors', tags={}, fields={}, bucket='rpi_thermo_chick', org='org'):
     try:
         write_api = client.write_api(write_options=SYNCHRONOUS)
-        name = point.get('name', 'tempSensors')
-        tags = ','.join([f'{k}={v}' for k,v in point.get('tags').items()])
-        fields = ','.join([f'{k}={v}' for k,v in point.get('fields').items()])
-        write_api.write(bucket, org, f'{name},{tags} {fields}')
+        tags_flat = ',' + ','.join([f'{k}={v}' for k,v in tags.items()]) if tags else ''
+        fields_flat = ','.join([f'{k}={v}' for k,v in fields.items()])
+        write_api.write(bucket, org, f'{name}{tags_flat} {fields_flat}')
+    except Exception as e:
+        logger.warning(f'cannot write influxdb client, exception({e})')
+
+def query_mean(name='tempSensors', field='inside', range='24h', window='15m', bucket='rpi_thermo_chick', org='org'):
+    try:
+        query_api = client.query_api()
+
+        query = (f'from(bucket:"{bucket}")\n'
+        f'|> range(start: -{range})\n'
+        f'|> filter(fn:(r) => r._measurement == "{name}")\n'
+        f'|> filter(fn: (r) => r["_field"] == "{field}")\n'
+        f'|> aggregateWindow(every: {window}, fn: mean, createEmpty: false)\n'
+        f'|> yield(name: "mean")')
+
+        result = query_api.query(org=org, query=query)
+
+        times = []
+        records = []
+        for table in result:
+            for record in table.records:
+                times.append(record.get_time().isoformat())
+                records.append(record.get_value())
+
+        return (times, records)
+
     except Exception as e:
         logger.warning(f'cannot write influxdb client, exception({e})')
 
